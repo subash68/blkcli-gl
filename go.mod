006a9bf9fe5243c9dd948afef32245d8b5b23761
Subash Chandra Bose <b.subi68@gmail.com>
2025-02-01 17:04:06 +0800

    Add modified main

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/subash68/blkcli-gl
+
+go 1.23.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,34 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
 
-func filter(numbers []int, f func(int) bool) []int {
+	"github.com/gorilla/mux"
+)
 
-	var result []int
-	for _, value := range numbers {
+// Define product here
+type Product struct {
+	Id       int
+	Name     string
+	Quantity int
+	Price    float64
+}
 
-		if f(value) {
-			result = append(result, value)
+var products []Product
+
+func getAllProducts(w http.ResponseWriter, req *http.Request) {
+	log.Println("Getting All products")
+	json.NewEncoder(w).Encode(products)
+}
+
+func getProductDetails(w http.ResponseWriter, req *http.Request) {
+	queryParams := mux.Vars(req)
+	id := queryParams["id"]
+
+	for _, item := range products {
+		itemId, _ := strconv.Atoi(id)
+		if item.Id == itemId {
+			json.NewEncoder(w).Encode(item)
 		}
 	}
-
-	return result
 }
 
-func mapSquare(numbers []int, f func(int) int) []int {
-	result := make([]int, len(numbers))
-
-	for index, value := range numbers {
-		result[index] = f(value)
-	}
-
-	return result
-}
+func AddProduct(w http.ResponseWriter, req *http.Request) {}
 
 func main() {
-	numbers := []int{10, 3, 4, 5, 6, 7}
 
-	squares := mapSquare(numbers, func(x int) int { return x * x })
-	fmt.Println(squares)
+	products = []Product{
+		Product{Id: 1, Name: "Product1", Quantity: 100, Price: 10.34},
+		Product{Id: 2, Name: "Product2", Quantity: 1000, Price: 1.55},
+	}
 
+	// Define router here
+	router := mux.NewRouter().StrictSlash(true) // FIXME: what does the strict slash does ?
+
+	router.HandleFunc("/products", getAllProducts)
+	router.HandleFunc("/product/{id}", getProductDetails)
+	http.ListenAndServe(":8000", router)
 }
